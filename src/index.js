bc1d4fe5d94ccd08999df0731535ad825ef971d3
kenny <kenny.kwyiu@gmail.com>
2023-12-31 22:45:40 +0800

    implement simple functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+import "./index.css";
 
 function App() {
   return (
@@ -17,11 +17,47 @@ function App() {
   );
 }
 
-function Avatar() {}
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="828a7b88-8569-4458-82f2-310bb4205395.png"
+      alt="Kenny Yiu"
+    />
+  );
+}
 
-function Intro() {}
+function Intro() {
+  return (
+    <duv>
+      <h1>Kenny Yiu</h1>
+      <p>
+        I am a self-motivated and dedicated person with great passion developing
+        innovative applications. I am a problem-solving lover and capable of
+        catching up with the latest tech knowledge in the market.
+      </p>
+    </duv>
+  );
+}
 
-function SkillList() {}
+function SkillList() {
+  return (
+    <div>
+      <Skill skill="Java" emoji="👍" />
+      <Skill skill="Spring Boot" emoji="🤖" />
+      <Skill skill="JavaScript" emoji="🐽" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill">
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
