6548a28cdf84ec64b7425dc9190138e716ebad3a
kenny <kenny.kwyiu@gmail.com>
2024-01-09 20:50:54 +0800

    create new challenge - flash card

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import DateCounter from "./DateCounter";
+import FlashCards from "./FlashCards";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <DateCounter />
+    <FlashCards />
   </StrictMode>
 );
