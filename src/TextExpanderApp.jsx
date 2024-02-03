e36cf34ee5297457bfdd737fd7bf970a076b2376
kenny <kenny.kwyiu@gmail.com>
2024-02-03 17:58:09 +0800

    121-commit teh change

--- src/TextExpanderApp.jsx
+++ src/TextExpanderApp.jsx
@@ -35,6 +35,23 @@ export default function TextExpanderApp() {
   );
 }
 
-function TextExpander() {
-  return <div>TESTING</div>;
+function TextExpander({
+  collapsedNumWords,
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+  buttonColor,
+  expanded = false,
+  className,
+  children,
+}) {
+  const [isExpanded, setIsExpanded] = useState(expanded);
+  const displayText = isExpanded ? children : "test";
+  return (
+    <div className={className}>
+      <span>{displayText}</span>
+      <button onClick={() => setIsExpanded(true)}>
+        {isExpanded ? collapseButtonText : expandButtonText}
+      </button>
+    </div>
+  );
 }
