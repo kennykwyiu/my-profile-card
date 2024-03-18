ef555f1b2a80ff7b5073d6157ab57715902bd9f6
kenny <kenny.kwyiu@gmail.com>
2024-03-18 21:53:50 +0800

    173-finished

--- src/Geolocation.js
+++ src/Geolocation.js
@@ -1,18 +1,11 @@
 import { useState } from "react";
 
-function useGeolocation() {}
-
-export default function Geolocation() {
-  const [countClicks, setCountClicks] = useState(0);
+function useGeolocation() {
   const [isLoading, setIsLoading] = useState(false);
   const [position, setPosition] = useState({});
   const [error, setError] = useState(null);
 
-  const { lat, lng } = position;
-
   function getPosition() {
-    setCountClicks((count) => count + 1);
-
     if (!navigator.geolocation)
       return setError("Your browser does not support geolocation");
 
@@ -32,9 +25,26 @@ export default function Geolocation() {
     );
   }
 
+  return { isLoading, position, error, getPosition };
+}
+
+export default function Geolocation() {
+  const {
+    isLoading,
+    position: { lat, lng },
+    error,
+    getPosition,
+  } = useGeolocation();
+  const [countClicks, setCountClicks] = useState(0);
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
   return (
     <div>
-      <button onClick={getPosition} disabled={isLoading}>
+      <button onClick={handleClick} disabled={isLoading}>
         Get my position
       </button>
 
